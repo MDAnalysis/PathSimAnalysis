053cbe8ba0f111e94f52313c3c94045042414d4e
Ian Kenney <ianmichaelkenney@gmail.com>
2023-10-04 16:55:50 -0700

    Made tests pass

--- pathsimanalysis/__init__.py
+++ pathsimanalysis/__init__.py
@@ -4,6 +4,15 @@ Calculates the geometric similary of molecular dynamics trajectories using path 
 """
 
 # Add imports here
+from .psa import *
+
+from MDAnalysis.due import due, Doi
+
+due.cite(Doi("10.1371/journal.pcbi.1004568"),
+         description="Path Similarity Analysis algorithm and implementation",
+         path="MDAnalysis.analysis.psa",
+         cite_module=True)
+del Doi
 
 # Handle versioneer
 from ._version import get_versions
